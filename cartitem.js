9df36e68460c14d9133cf99e5479dbf83e16c627
Manisha-95 <raosmanisha@gmail.com>
2020-04-20 17:31:16 +0530

    2 files modified

--- cartitem.js
+++ cartitem.js
@@ -11,20 +11,24 @@ class CartItem extends Component {
     return (
       <div className="container">
            <div className="desc row">
-      
+   
         <img src={this.props.item.name} width="50px"/>
+  
       
-        <p>{this.props.item.description}</p>
-        <p>{this.props.item.itemQuan}</p>
-        <p>{this.props.item.price}</p>
+        <p>{this.props.item.description}<br/>
+        {this.props.item.itemQuan}<br/>
+        {this.props.item.price}</p>
+   
         
  <br/>
+ 
         <button className="btn" onClick={()=> this.props.onIncrement(this.props.item)} >+</button>
         <button className="btn"disabled={this.props.item.quantity===0} onClick={()=> this.props.onDecrement(this.props.item)}  >-</button>
         
         <button className="btn" >{this.props.item.quantity}</button>
         <button onClick={()=>this.props.addtocart()}>Add to Cart</button>
         </div>
+        
       </div>
     );
   }
