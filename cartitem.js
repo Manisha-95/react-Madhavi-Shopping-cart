e00dcfe58dda456155c7d9e85146753a72c9b67f
Manisha-95 <raosmanisha@gmail.com>
2020-04-20 17:54:13 +0530

    1 file modified

--- cartitem.js
+++ cartitem.js
@@ -11,22 +11,26 @@ class CartItem extends Component {
     return (
       <div className="container">
            <div className="desc row">
-   
-        <img src={this.props.item.name} width="50px"/>
-  
-      
-        <p>{this.props.item.description}<br/>
-        {this.props.item.itemQuan}<br/>
-        {this.props.item.price}</p>
-   
+           <div class="d-flex bd-highlight">
+  <div class="p-2 flex-grow-1 bd-highlight"><img  src={this.props.item.name} width="50px"/></div>
+  <div class="p-2 bd-highlight align-self-stretch">
+  <div class="d-flex flex-column bd-highlight mb-3">
+  <div class="p-2 bd-highlight">{this.props.item.description}</div>
+  <div class="p-2 bd-highlight">{this.props.item.itemQuan}</div>
+  <div class="p-2 bd-highlight">{this.props.item.price}</div>
+</div>
+  <p>
         
- <br/>
- 
+        </p>
+   </div>
+   <div class="p-2 bd-highlight align-self-center">
         <button className="btn" onClick={()=> this.props.onIncrement(this.props.item)} >+</button>
         <button className="btn"disabled={this.props.item.quantity===0} onClick={()=> this.props.onDecrement(this.props.item)}  >-</button>
         
         <button className="btn" >{this.props.item.quantity}</button>
         <button onClick={()=>this.props.addtocart()}>Add to Cart</button>
+        </div>
+</div> 
         </div>
         
       </div>
