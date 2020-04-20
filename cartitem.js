edae9cffd2773b561351c7b13ae4d665b19fa867
Manisha-95 <raosmanisha@gmail.com>
2020-04-20 18:56:32 +0530

    1 file modified

--- cartitem.js
+++ cartitem.js
@@ -10,28 +10,35 @@ class CartItem extends Component {
   render() {
     return (
       <div className="container">
-           <div className="desc row">
-           <div class="d-flex bd-highlight">
-  <div class="p-2 flex-grow-1 bd-highlight"><img  src={this.props.item.name} width="50px"/></div>
-  <div class="p-2 bd-highlight align-self-stretch">
+           <div className="desc row justify-content-center">
+           <div class="d-flex">
+  <div class="p-2 flex-grow-1 bd-highlight align-self-center"><img  src={this.props.item.name} width="200px"/></div>
+  <div class="p-2 bd-highlight">
   <div class="d-flex flex-column bd-highlight mb-3">
   <div class="p-2 bd-highlight">{this.props.item.description}</div>
   <div class="p-2 bd-highlight">{this.props.item.itemQuan}</div>
   <div class="p-2 bd-highlight">{this.props.item.price}</div>
 </div>
-  <p>
-        
-        </p>
+  
    </div>
-   <div class="p-2 bd-highlight align-self-center">
-        <button className="btn" onClick={()=> this.props.onIncrement(this.props.item)} >+</button>
-        <button className="btn"disabled={this.props.item.quantity===0} onClick={()=> this.props.onDecrement(this.props.item)}  >-</button>
-        
-        <button className="btn" >{this.props.item.quantity}</button>
-        <button onClick={()=>this.props.addtocart()}>Add to Cart</button>
-        </div>
+  
 </div> 
+
+</div>
+
+<div class="d-flex">
+  <div class="p-2 bd-highlight">18% OFF</div>
+  <div class="p-2 bd-highlight"><div className="round" onClick={()=> this.props.onIncrement(this.props.item)} >+</div>
+  <div className="btn" >{this.props.item.quantity}</div>
+      <div className="round" disabled={this.props.item.quantity===0} onClick={()=> this.props.onDecrement(this.props.item)}>-</div>
+      
+  </div>
+  <div class="p-2 bd-highlight"> <button onClick={()=>this.props.addtocart()}>Add to Cart</button></div>
+  
         </div>
+
+ 
+      
         
       </div>
     );
