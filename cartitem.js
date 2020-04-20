96c9c54a44f43c6d56ba5ba468e6fbe7d19a5650
Manisha-95 <raosmanisha@gmail.com>
2020-04-20 17:00:27 +0530

    1 file modified

--- cartitem.js
+++ cartitem.js
@@ -10,7 +10,22 @@ class CartItem extends Component {
   render() {
     return (
       <div className="container">
+      /* <div class="card mb-3 container">
+  <div class="row no-gutters">
+    <div class="col-lg-4">
+      <img src={this.props.item.name} width="50px"/>
+    </div>
+    <div class="col-lg-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div> */
         <div className="desc">
+      
         <img src={this.props.item.name} width="50px"/>
       
         <p>{this.props.item.description}</p>
