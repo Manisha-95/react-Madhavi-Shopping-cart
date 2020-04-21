d22e5068aa343a88fab8ad3ffaaf5dc9f250f748
Manisha-95 <raosmanisha@gmail.com>
2020-04-21 13:38:00 +0530

    2 files modified

--- cart.js
+++ cart.js
@@ -6,6 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import  FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faTumblr, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 class Cart extends Component {
   constructor() {
@@ -89,7 +91,31 @@ class Cart extends Component {
                 <span className="btn btn-info">Cart items: {this.state.cartItemsCount}</span>
                 <span className="btn btn-info">Cart total:{this.state.cartTotal}</span>
                 <span title="Reset Cart" className="btn btn-info" onClick={this.onResetHandler}>reset</span>
-                <span className="checkout btn btn-info">Checkout</span>
+               
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
           </div>   
           {
             this.state.items.map(item=> 
