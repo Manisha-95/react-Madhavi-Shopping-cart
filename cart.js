147daf05c8732c358849ebf81820ea684f2908fa
Manisha-95 <raosmanisha@gmail.com>
2020-04-21 15:47:14 +0530

    2 files modified

--- cart.js
+++ cart.js
@@ -93,26 +93,25 @@ class Cart extends Component {
                 <span title="Reset Cart" className="btn btn-info" onClick={this.onResetHandler}>reset</span>
                
 
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
+<button type="button" class="btn btn-primary mod" data-toggle="modal" data-target="#exampleModal">
+  Checkout
 </button>
 
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Thank You For Shopping</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        <h4>Total Amount: {this.state.cartTotal}</h4>
+
+        !!!!Transaction Successful.!!!!
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+    
     </div>
   </div>
 </div>
