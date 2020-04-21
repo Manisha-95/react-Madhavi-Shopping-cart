fb00586de74cedf078b9be798b014ff554695693
Manisha-95 <raosmanisha@gmail.com>
2020-04-21 15:36:21 +0530

    2 files modified

--- cart.js
+++ cart.js
@@ -92,31 +92,30 @@ class Cart extends Component {
                 <span className="btn btn-info">Cart total:{this.state.cartTotal}</span>
                 <span title="Reset Cart" className="btn btn-info" onClick={this.onResetHandler}>reset</span>
                
-<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
   Launch demo modal
 </button>
 
-
-<div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div className="modal-dialog" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div className="modal-body">
-      
+      <div class="modal-body">
+        ...
       </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
 </div>
-
           </div>   
           {
             this.state.items.map(item=> 
